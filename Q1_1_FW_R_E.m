641132b8a6fecfd81798bdc45c5a24fe4a7032b6
ptundra <95395090+ptundra@users.noreply.github.com>
2023-11-18 23:37:01 -0500

    Update Q1_1_FW_R_E.m

--- Q1_1_FW_R_E.m
+++ Q1_1_FW_R_E.m
@@ -2,5 +2,7 @@
 % Pouya Asgharzadeh, Vic Gao, Min Woo (David) Kong
 % 1.1 Fixed-Wing UAS Development
 
+Fuel_Cap = properties.Fuel_Cap_L; % amount of fuel in L
+
 AR = properties.wing_span/properties.chord_length;
 K = 1 / (pi*AR*properties.efficiency_factor);
