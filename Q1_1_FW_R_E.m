8d9f32cc773108ef35fabdbfa08a11ca1782885b
ptundra <95395090+ptundra@users.noreply.github.com>
2023-11-20 02:35:57 -0500

    Update Q1_1_FW_R_E.m

--- Q1_1_FW_R_E.m
+++ Q1_1_FW_R_E.m
@@ -2,6 +2,23 @@
 % Pouya Asgharzadeh, Vic Gao, Min Woo (David) Kong
 % 1.1 Fixed-Wing UAS Development
 
+% Import altitude and density data from a text file
+% https://www.eoas.ubc.ca/courses/atsc113/flying/met_concepts/03-met_concepts/03a-std_atmos/index.html
+air_atm = importdata('air_atm.txt');
+h_data = air_atm.data(1:end, 1);  % altitude data (first column)
+temp_data = air_atm.data(1:end, 2);  % temperature data (second column)
+press_data = air_atm.data(1:end, 3);  % pressure data (third column)
+rho_data = air_atm.data(1:end, 4);  % air density data (fourth column)
+
+for i = 1:length(h_data)
+    height_curr = h_data(i);
+    temp_curr = temp_data(i);
+    press_curr = press_data(i);
+    rho_curr = rho_data(i);
+
+    R = press_curr / (0.2869 * (temp_curr + 273.1));
+end
+
 % https://laws-lois.justice.gc.ca/eng/regulations/sor-96-433/page-56.html
 % assumed altitude of the flight above sea level in (m)
 Altitude = 500; % for aeroplanes, 1,000 feet above
