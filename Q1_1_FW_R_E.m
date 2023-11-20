975965ef1e02d352879262fcd6ce6a21590cd63b
ptundra <95395090+ptundra@users.noreply.github.com>
2023-11-20 02:07:07 -0500

    Update Q1_1_FW_R_E.m

--- Q1_1_FW_R_E.m
+++ Q1_1_FW_R_E.m
@@ -2,11 +2,15 @@
 % Pouya Asgharzadeh, Vic Gao, Min Woo (David) Kong
 % 1.1 Fixed-Wing UAS Development
 
+% https://laws-lois.justice.gc.ca/eng/regulations/sor-96-433/page-56.html
+% assumed altitude of the flight above sea level in (m)
+Altitude = 500; % for aeroplanes, 1,000 feet above
+
 Fuel_Cap = properties.fuel_cap; % amount of fuel in kg
 
 % Jet A-1 is a type of aviation fuel commonly used
-Fuel_Density = (775.0 + 840.0) / 2; % density of Jet A-1 fuel (kg/m^3)
 % https://code7700.com/fuel_density.htm
+Fuel_Density = (775.0 + 840.0) / 2; % density of Jet A-1 fuel (kg/m^3)
 
 % weight of the fuel in N
 W_fuel_full = Fuel_Cap * Fuel_density * P.gravity;
